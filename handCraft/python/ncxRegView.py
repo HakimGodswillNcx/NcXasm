9bd179afe68f0105c5e381a331ca6268840a6562
HakimGodSwillNcx <141035139+HakimGodswillNcx@users.noreply.github.com>
2023-07-31 20:16:04 +0100

    Update ncxRegView.py

--- handCraft/python/ncxRegView.py
+++ handCraft/python/ncxRegView.py
@@ -1,42 +1,54 @@
+import sys
 import psutil
-import pydbg
+import subprocess
+import time
 
+def print_registers(pid):
+    print("here is pid attached function success")
+    gdbCMD='-ex "set pagination off" -ex "info registers" -ex "detach" -ex "quit"'
+    gdb_command = f"sudo gdb -p {pid}"+' '+gdbCMD
+
+    try:
+        gdb_process = subprocess.Popen(gdb_command, shell=True)
+        time.sleep(1)  # Adjust the delay as needed
+
+        # Optionally, you can add more GDB commands here
+
+        gdb_process.wait()
+        gdb_output, _ = gdb_process.communicate()
+        print(gdb_output)  # This will print GDB's output
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error while running GDB: {e}")
 def find_proc_by_name(proc_name):
     for proc in psutil.process_iter(['pid', 'name']):
         if proc_name.lower() in proc.info['name'].lower():
             return proc.info['pid']
     return None
-
-def print_registers(dbg):
-    context = dbg.context
-    print("EAX: 0x{:08x}".format(context.Eax))
-    print("EBX: 0x{:08x}".format(context.Ebx))
-    print("ECX: 0x{:08x}".format(context.Ecx))
-    print("EDX: 0x{:08x}".format(context.Edx))
-    print("ESP: 0x{:08x}".format(context.Esp))
-    print("EBP: 0x{:08x}".format(context.Ebp))
-    print("ESI: 0x{:08x}".format(context.Esi))
-    print("EDI: 0x{:08x}".format(context.Edi))
-    print("EIP: 0x{:08x}".format(context.Eip))
-    # Additional registers can be accessed based on the target architecture.
-
+def get_all_process_names():
+    process_names = set()
+    for proc in psutil.process_iter(['name']):
+        process_names.add(proc.info['name'])
+    return process_names
 def main():
-    proc_name = input("Enter the process name to search for: ")
+    proc_name = input("Enter the [process_name]: ")
     pid = find_proc_by_name(proc_name)
 
     if pid is not None:
         print(f"Process '{proc_name}' found with PID: {pid}")
-        dbg = pydbg.pydbg()
         try:
-            dbg.attach(pid)
+            pid = int(input("Enter the PID of the process to inspect: "))
             print("Attached to process with PID: {}".format(pid))
-            print_registers(dbg)
+            print_registers(pid)
         except Exception as e:
             print("Error attaching to process:", e)
-        finally:
-            dbg.detach()
     else:
+        all_process_names = get_all_process_names()
+        print("List of all process names:")
+        for name in all_process_names:
+            print(name)
         print(f"No process with the name '{proc_name}' found.")
 
 if __name__ == "__main__":
     main()
+    print("system_exit")
